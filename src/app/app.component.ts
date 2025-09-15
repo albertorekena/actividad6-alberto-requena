5aa344d94ea0d346ea20b58a825974b8088bb85d
albertorekena <albertorekena.dev@gmail.com>
2025-09-15 07:30:37 +0200

    Working on the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,4 @@ import {FooterComponent} from "./components/footer/footer.component";
   templateUrl:"./app.component.html",
   styleUrl:"./app.component.css"
 })
-export class AppComponent {
-
-};
+export class AppComponent {};
