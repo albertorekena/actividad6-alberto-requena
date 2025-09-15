4a2e68fa308be44f48ea60c8459ba3dfb51a9399
albertorekena <albertorekena.dev@gmail.com>
2025-09-15 13:29:33 +0200

    Header component finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
-import {HeaderComponent} from "./components/header/header.component";
+import {HeaderComponent} from "./shared/header/header.component";
 import {RouterOutlet} from "@angular/router";
-import {FooterComponent} from "./components/footer/footer.component";
+import {FooterComponent} from "./shared/footer/footer.component";
 
 @Component({
   selector:"app-root",
