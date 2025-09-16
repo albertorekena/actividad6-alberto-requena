a6ba36186f4d8e84385e63d108bbfbfe4417aa30
albertorekena <albertorekena.dev@gmail.com>
2025-09-16 05:59:56 +0200

    User view page finished

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,10 +1,11 @@
-import {Component, inject} from "@angular/core";
+import {Component, inject, Input} from "@angular/core";
 import {UsersService} from "../../services/users.service";
 import {IUser} from "../../interfaces/iuser";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector:"app-user",
-  imports:[],
+  imports: [RouterLink],
   templateUrl:"./user.component.html",
   styleUrl:"./user.component.css"
 })
@@ -12,11 +13,16 @@ export class UserComponent {
   usersService = inject(UsersService);
   user:IUser | undefined;
 
+  @Input()
+  _id!:string;
+
   async ngOnInit() {
     try {
-      this.user = await this.usersService.show();
+      this.user = await this.usersService.show(this._id);
     } catch (error:any) {
       console.log(error);
     }
   }
+
+  removeUser(_id:string | undefined):void {}
 };
